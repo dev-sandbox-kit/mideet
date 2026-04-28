24eea04d948e81e1581f0888de8ccf7bb8204d1d
김나현 <nahkim@huray.net>
2026-04-28 18:15:26 +0900

    refactor: Update Posthog defaults

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -8,7 +8,7 @@ export default function PostHogProvider({ children }: { children: React.ReactNod
     if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) return
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-      capture_pageview: false,
+      defaults: '2026-01-30',
     })
   }, [])
 
