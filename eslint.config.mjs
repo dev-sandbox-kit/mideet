4f958a28b4a123b62c2c9c2bc1c60201b198a880
nahkim <dev.nahkim@gmail.com>
2026-04-30 13:17:10 +0900

    refactor: Clean up legacy code and add lint tooling

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
+
+export default [
+  {
+    ignores: [
+      '.next/**',
+      '.worktrees/**',
+      'node_modules/**',
+      'supabase/functions/**',
+      'next-env.d.ts',
+    ],
+  },
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react-hooks/set-state-in-effect': 'warn',
+    },
+  },
+]
